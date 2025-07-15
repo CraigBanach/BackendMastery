d68fc5b57c3d997a4f61463a12e8dab10bf31d42
Craig Banach <craigsb93@gmail.com>
2025-07-15 21:11:07 +0100

    Add perf testing

---
+++ PersonifiBackend/src/PersonifiBackend.Core/Interfaces/IPerformanceTestingService.cs
@@ -0,0 +1,11 @@
+﻿using PersonifiBackend.Core.Entities;
+
+namespace PersonifiBackend.Core.Interfaces;
+
+public interface IPerformanceTestingService
+{
+    Task<PerformanceReport> RunPerformanceTestsAsync(
+        string userId,
+        CancellationToken cancellationToken = default
+    );
+}
