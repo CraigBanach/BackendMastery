00375fd81bea62526e8d60e77274dad7c766d90c
Craig Banach <craigsb93@gmail.com>
2025-08-27 21:13:34 +0100

    Fix build issues (#59)

--- personifi-app/src/app/layout.tsx
+++ personifi-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/header";
+import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,16 +11,20 @@ export const metadata: Metadata = {
   description: "Personal Finance for your family",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headerList = await headers();
+  const pathName = headerList.get("x-current-path");
+  const isLandingPage = pathName === "/" || pathName === null;
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <Header></Header>
-        <main className="flex flex-col">{children}</main>
+        <main className={`flex flex-col ${isLandingPage ? '' : 'p-3 sm:p-4 md:p-6'}`}>{children}</main>
       </body>
     </html>
   );
--- personifi-app/src/components/layout/header.tsx
+++ personifi-app/src/components/layout/header.tsx
@@ -8,7 +8,7 @@ const Header = async () => {
   const isLandingPage = pathName === "/" || pathName === null;
   
   return (
-    <header className={`${isLandingPage ? 'bg-transparent relative z-50' : 'sticky top-0 bg-gradient-to-br from-finance-green-light/20 to-white z-50'} flex h-16 items-center gap-4 px-4 md:px-6`}>
+    <header className={`${isLandingPage ? 'bg-transparent relative z-50' : 'sticky top-0 bg-white border-b z-50'} flex h-16 items-center gap-4 px-4 md:px-6`}>
       <nav className="flex flex-1 items-center justify-between justify-items-end gap-4">
         <TopNavigation />
         <SessionHeader />
